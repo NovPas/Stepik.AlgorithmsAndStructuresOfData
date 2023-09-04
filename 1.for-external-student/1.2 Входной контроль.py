4bf92ee777b13a28cef11eaf17d290a8d2de8fd4
NovPas <novpas@gmail.com>
2023-09-04 21:48:51 +0300

    Initial commit

---
+++ 1.for-external-student/1.2 Входной контроль.py
@@ -0,0 +1,62 @@
+# 1. Триады
+# Ответ 111000101
+
+# 2. Строковый градусник
+# x = int(input())
+# if x < -25:
+#     result = 'жутко холодно'
+# elif -26 < x < 0:
+#     result = 'холодно'
+# elif -1 < x < 10:
+#     result = 'прохладно'
+# elif 9 < x < 25:
+#     result = 'тепло'
+# elif 24 < x:
+#     result = 'жара'
+#
+# print(result)
+
+
+# 3. Любое чётное
+# line = input()
+# list_substrings = line.split()
+# # print(list_substrings)
+# for x in list_substrings:
+#     if int(x) % 2 == 0:
+#         result = x
+#         break
+#
+# print(result)
+
+
+# 4. Минимальное число
+# min_element = 30000
+# number_count = input()
+# for x in range(int(number_count)):
+#     element = input()
+#     if int(element) < min_element and element.endswith('3'):
+#         min_element = int(element)
+#
+# print(min_element)
+
+
+# 5. Чётные против нечётных
+even = 0
+odd = 0
+line = input()
+list_substrings = line.split()
+
+for x in list_substrings:
+    if int(x) % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+if even > odd:
+    result = 'even'
+elif even < odd:
+    result = 'odd'
+else:
+    result = 'equal'
+
+print(result)
---
+++ 2.Разминка/2.1 Арифметика.py
@@ -0,0 +1,25 @@
+import math
+
+# 1. Свой-Чужой
+# import math
+#
+# n = input()
+# if n > 0:
+#     result = sum(range(1, n + 1))
+# else:
+#     result = sum(range(1, n - 1, -1))
+# print(result)
+
+
+# 2. Прямоугольники
+# count = 0
+# rect_square = int(input())
+# max_side = int(math.sqrt(rect_square))
+# for x in range(1, max_side+1):
+#     if rect_square % x == 0:
+#         count += 1
+#
+# print(count)
+
+
+# 3. Результаты охоты
